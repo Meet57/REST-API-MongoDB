88913bb24ed8007192fcad800a6834af79ad44f9
Meet57 <meet57.mp@gmail.com>
2021-05-06 01:08:06 +0530

    undo

--- server.js
+++ server.js
@@ -11,16 +11,13 @@ app.use(express.json())
 const postsRoutes = require('./routes/api/posts')
 
 //Connect to MOngo db
-mongoose.connect('mongodb+srv://meet:meet@meet.soyef.mongodb.net/canblog?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
     .then(() => {
         console.log('MongoDB Connected');
     })
 
 //use Routes
 app.use('/api/posts', postsRoutes);
-app.get('/',(req, res) => {
-    res.send('Home').status(200)
-})
 
 const PORT = process.env.YOUR_PORT || process.env.PORT || 80
 app.listen(PORT, () => { console.log(`server at ${PORT}`); })
